6428769c1acd5d631684529fe775c5ad133b651a
sunnylin0 <sunny.lin@minaik.com.tw>
2024-05-14 00:36:22 +0800

    加入專案檔案。

---
+++ Class1.cs
@@ -0,0 +1,120 @@
+﻿
+using System;
+using AutoCAD;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+namespace AcadExample
+{
+    public static class Marshal2
+    {
+        internal const String OLEAUT32 = "oleaut32.dll";
+        internal const String OLE32 = "ole32.dll";
+
+        [System.Security.SecurityCritical]  // auto-generated_required
+        public static Object GetActiveObject(String progID)
+        {
+            Object obj = null;
+            Guid clsid;
+
+            // Call CLSIDFromProgIDEx first then fall back on CLSIDFromProgID if
+            // CLSIDFromProgIDEx doesn't exist.
+            try
+            {
+                CLSIDFromProgIDEx(progID, out clsid);
+            }
+            //            catch
+            catch (Exception)
+            {
+                CLSIDFromProgID(progID, out clsid);
+            }
+
+            GetActiveObject(ref clsid, IntPtr.Zero, out obj);
+            return obj;
+        }
+
+        //[DllImport(Microsoft.Win32.Win32Native.OLE32, PreserveSig = false)]
+        [DllImport(OLE32, PreserveSig = false)]
+        //[ResourceExposure(ResourceScope.None)]
+        //[SuppressUnmanagedCodeSecurity]
+        [System.Security.SecurityCritical]  // auto-generated
+        private static extern void CLSIDFromProgIDEx([MarshalAs(UnmanagedType.LPWStr)] String progId, out Guid clsid);
+
+        //[DllImport(Microsoft.Win32.Win32Native.OLE32, PreserveSig = false)]
+        [DllImport(OLE32, PreserveSig = false)]
+        //[ResourceExposure(ResourceScope.None)]
+        //[SuppressUnmanagedCodeSecurity]
+        [System.Security.SecurityCritical]  // auto-generated
+        private static extern void CLSIDFromProgID([MarshalAs(UnmanagedType.LPWStr)] String progId, out Guid clsid);
+
+        //[DllImport(Microsoft.Win32.Win32Native.OLEAUT32, PreserveSig = false)]
+        [DllImport(OLEAUT32, PreserveSig = false)]
+        //[ResourceExposure(ResourceScope.None)]
+        //[SuppressUnmanagedCodeSecurity]
+        [System.Security.SecurityCritical]  // auto-generated
+        private static extern void GetActiveObject(ref Guid rclsid, IntPtr reserved, [MarshalAs(UnmanagedType.Interface)] out Object ppunk);
+
+    }
+    public class AutoCADConnector : IDisposable
+    {
+        private AcadApplication _application;
+        private bool _initialized;
+        private bool _disposed;
+        public AutoCADConnector()
+        {
+            try
+            {
+                   // Upon creation, attempt to retrieve running instance
+                   _application = (AcadApplication)Marshal2.GetActiveObject("AutoCAD.Application");
+            }
+            catch
+            {
+                try
+                {
+                    // Create an instance and set flag to indicate this
+                    _application = new AcadApplicationClass();
+                    _initialized = true;
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+        // If the user doesn‘t call Dispose, the
+        // garbage collector will upon destruction
+        ~AutoCADConnector()
+        {
+            Dispose(false);
+        }
+
+        public AcadApplication Application
+        {
+            get
+            {
+                // Return our internal instance of AutoCAD
+                return _application;
+            }
+        }
+
+        // This is the user-callable version of Dispose.
+        // It calls our internal version and removes the
+        // object from the garbage collector‘s queue.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // This version of Dispose gets called by our
+        // destructor.
+        protected virtual void Dispose(bool disposing)
+        {
+            // If we created our AutoCAD instance, call its
+            // Quit method to avoid leaking memory.
+            if (!this._disposed && _initialized)
+                _application.Quit();
+
+            _disposed = true;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AutoCAD;
+
+namespace autocad_part2
+{
+    public partial class Form1 : Form
+    {
+        //private AcadApplication a;//聲明AutoCAD對象
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AcadExample.AutoCADConnector acadapp = new AcadExample.AutoCADConnector();
+            AcadDocument a = acadapp.Application.ActiveDocument;
+            
+            
+            
+            double[] startPoint = new double[3]; //聲明直線起點座標
+            double[] endPoint = new double[3];//聲明直線終點座標
+            //string[] str = textBox1.Text.Split(',');//取出直線起點座標輸入文字框的值，文字框的輸入模式為＂x,y,z＂
+            //for (int i = 0; i < 3; i++)
+            //    startPoint[i] = Convert.ToDouble(str[i]);//將str數組轉為double型 
+            //str = textBox2.Text.Split(',');//取出直線終點座標輸入文字框的值
+            //for (int i = 0; i < 3; i++)
+            //    endPoint[i] = Convert.ToDouble(str[i]);
+            startPoint[0] = 44;
+            startPoint[1] = 44;
+            startPoint[2] = 0;
+            endPoint[0] = 55;
+            endPoint[1] = 155;
+            endPoint[2] = 0;
+            a.ModelSpace.AddLine(startPoint, endPoint);//在AutoCAD中畫直線
+            a.Application.Update();//更新顯示
+        }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    double[] startPoint = new double[3]; //聲明直線起點座標
+        //    double[] endPoint = new double[3];//聲明直線終點座標
+        //    string[] str = textBox1.Text.Split(',');//取出直線起點座標輸入文字框的值，文字框的輸入模式為＂x,y,z＂
+        //    for (int i = 0; i < 3; i++)
+        //        startPoint[i] = Convert.ToDouble(str[i]);//將str數組轉為double型 
+        //    str = textBox2.Text.Split(',');//取出直線終點座標輸入文字框的值
+        //    for (int i = 0; i < 3; i++)
+        //        endPoint[i] = Convert.ToDouble(str[i]);
+        //    a.ActiveDocument.ModelSpace.AddLine(startPoint, endPoint);//在AutoCAD中畫直線
+        //    a.Application.Update();//更新顯示
+        //}
+
+
+    }
+}
